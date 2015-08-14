84d0ad52714050f206758e0093ce56305af34ee2
jackiemarchal <jackie.marchal@aol.com>
2015-08-14 16:50:54 +0000

    hello

---
+++ methods.js
@@ -0,0 +1,101 @@
+$(function(){
+  
+  $(".high").mouseleave(function(){
+   $("#high").hide();  
+  });
+  
+$(".high").mouseenter(function(){
+   $("#high").text("In High Spirits").show().addClass("pretty");  
+  });
+  
+  $(".looking").mouseleave(function(){
+   $("#looking").hide();  
+  });
+  
+$(".looking").mouseenter(function(){
+   $("#looking").text("Searching for a F*@k to Give").show().addClass("pretty");  
+  });
+	
+  $(".horn-ey").mouseleave(function(){
+   $("#horn-ey").hide();  
+  });
+  
+$(".horn-ey").mouseenter(function(){
+   $("#horn-ey").text("Feeling Jazzy").show().addClass("pretty");  
+  });
+	
+  $(".nightout").mouseleave(function(){
+   $("#nightout").hide();  
+  });
+  
+$(".nightout").mouseenter(function(){
+   $("#nightout").text("Night Out").show().addClass("pretty");  
+  });
+  
+    $(".pregame").mouseleave(function(){
+   $("#pregame").hide();  
+  });
+  
+$(".pregame").mouseenter(function(){
+   $("#pregame").text("Pregame").show().addClass("pretty");  
+  });
+  
+  $(".breakup").mouseleave(function(){
+    $("#breakup").hide();  
+   });
+  
+ $(".breakup").mouseenter(function(){
+    $("#breakup").text("Breakup").show().addClass("pretty");  
+  });
+  
+    $(".angry").mouseleave(function(){
+   $("#angry").hide();  
+  });
+  
+$(".angry").mouseenter(function(){
+   $("#angry").text("Angry").show().addClass("pretty");  
+  });
+  
+  $(".happy").mouseleave(function(){
+   $("#happy").hide();  
+  });
+  
+$(".happy").mouseenter(function(){
+   $("#happy").text("Happy").show().addClass("pretty");  
+  });
+	
+  $(".unsure").mouseleave(function(){
+   $("#unsure").hide();  
+  });
+  
+$(".unsure").mouseenter(function(){
+   $("#unsure").text("Emotionally Lost").show().addClass("pretty");  
+  });
+	
+  $(".love").mouseleave(function(){
+   $("#love").hide();  
+  });
+  
+$(".love").mouseenter(function(){
+   $("#love").text("Love").show().addClass("pretty");  
+  });
+  
+    $(".confident").mouseleave(function(){
+   $("#confident").hide();  
+  });
+  
+$(".confident").mouseenter(function(){
+   $("#confident").text("Confident").show().addClass("pretty");  
+  });
+  
+   $(".sad").mouseleave(function(){
+   $("#sad").hide();  
+  });
+  
+$(".sad").mouseenter(function(){
+   $("#sad").text("Sad").show().addClass("pretty");  
+  });
+    
+  
+
+});
